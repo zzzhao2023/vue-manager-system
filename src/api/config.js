89e4baf7a1382e9120e364d2b5590557c976a755
wangzhaorui <wangzhaorui@tongqianguan.com>
2019-06-10 16:10:31 +0800

    初始化题库管理

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,1 @@
-//axios基础配置文件
-export const ERR_OK = 200;
-
-export const Teacher = 0;
-
-export const Student = 1;
-
-export const BASE_URL = "http://59.110.212.84:9999";
-
-export const FILE_URL = "http://192.168.108.236";
+//axios基础配置文件export const ERR_OK = 200;export const Teacher = 0;export const Student = 1;export const BASE_URL = "http://59.110.212.84:9999";export const FILE_URL = "http://192.168.108.236";
